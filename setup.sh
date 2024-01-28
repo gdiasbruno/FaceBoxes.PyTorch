91c379852fcbadc39c9f47f1f0a964ac9971a404
gdiasbruno <gdias.bruno@gmail.com>
2024-01-28 17:03:55 +0000

    Fix setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
-pip install requirements.txt
+pip install -r requirements.txt
 
 bash make.sh
+
+pip install gdown
 
 gdown https://drive.google.com/uc?id=1AKntcQlxHGNBJOhfFjS2TIlLKfOHCFjs
 
@@ -16,3 +18,6 @@ gdown https://drive.google.com/uc?id=1FAfy-2X2Ly_HiimafwTLtpiRVgOlX7yV
 apt-get update && apt-get install unzip
 
 unzip all_pics.zip
+
+# $ git config --global user.name "gdiasbruno"
+# $ git config --global user.email gdias.bruno@gmail.com
